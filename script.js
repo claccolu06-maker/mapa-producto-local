f1c6feb3e8a80e01bfb05b27603721bf7d393662
claccolu06-maker <claccolu06@gmail.com>
2026-02-04 18:20:04 +0100

    Enhance map markers with category icons and info
    Added additional icons for different categories and updated the popup to include the category of each location.

--- script.js
+++ script.js
@@ -28,7 +28,53 @@ var iconoEspanol = L.icon({
     shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
     shadowSize: [41, 41]
 });
+// Icono por defecto (frutería)
+var iconoFruteria = L.icon({
+  iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+  shadowSize: [41, 41]
+});
 
+// Otros colores
+var iconoPanaderia = L.icon({
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-orange.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+  shadowSize: [41, 41]
+});
+
+var iconoCarniceria = L.icon({
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+  shadowSize: [41, 41]
+});
+
+var iconoOtros = L.icon({
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-grey.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+  shadowSize: [41, 41]
+});
+
+// Icono verde para “producto español” (solo fruterías, si quieres mantenerlo)
+var iconoEspanol = L.icon({
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+  shadowSize: [41, 41]
+});
 // Cargar locales desde el JSON
 fetch('data/locales.json')
   .then(response => {
@@ -40,25 +86,37 @@ fetch('data/locales.json')
   .then(locales => {
     console.log("Locales cargados:", locales);
 
-    locales.forEach(local => {
-      const textoOrigen = local.origen_espanol
-        ? '✔ Producto español'
-        : '✖ Producto español no verificado';
+  locales.forEach(local => {
+  const textoOrigen = local.origen_espanol
+    ? '✔ Producto español'
+    : '✖ Producto español no verificado';
 
-      const popupHtml = `
-        <strong>${local.nombre}</strong><br>
-        ${local.direccion}<br>
-        ${textoOrigen}<br>
-        <a href="${local.google_maps}" target="_blank">Abrir en Google Maps</a>
-      `;
+  const popupHtml = `
+    <strong>${local.nombre}</strong><br>
+    ${local.direccion}<br>
+    Categoría: ${local.categoria}<br>
+    ${textoOrigen}<br>
+    <a href="${local.google_maps}" target="_blank">Abrir en Google Maps</a>
+  `;
 
-      const icono = local.origen_espanol ? iconoEspanol : iconoNormal;
+  let icono;
 
-      L.marker([local.lat, local.lng], { icon: icono })
-        .addTo(map)
-        .bindPopup(popupHtml);
-    });
+  if (local.categoria === 'fruteria') {
+    icono = local.origen_espanol ? iconoEspanol : iconoFruteria;
+  } else if (local.categoria === 'panaderia') {
+    icono = iconoPanaderia;
+  } else if (local.categoria === 'carniceria') {
+    icono = iconoCarniceria;
+  } else {
+    icono = iconoOtros;
+  }
+
+  L.marker([local.lat, local.lng], { icon: icono })
+    .addTo(map)
+    .bindPopup(popupHtml);
+});
   })
   .catch(error => {
     console.error(error);
   });
+
